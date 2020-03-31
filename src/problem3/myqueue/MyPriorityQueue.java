6be0acfc943f82c65423f75a6457c5f7ccfff8b2
MansiKukreja24 <55954224+MansiKukreja24@users.noreply.github.com>
2020-03-31 19:10:26 +0530

    Update MyPriorityQueue.java

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -6,5 +6,36 @@
  */
 package problem3.myqueue;
 
+
+
+import problem3.myLinkedList.LinkedList;
+
+import problem3.student.Student;
+
+
+
 public class MyPriorityQueue {
+
+    LinkedList linkedList = new LinkedList();
+
+    public void enqueue(Student student){
+
+        linkedList.addLast(student);
+
+    }
+
+
+
+    public void dequeue(){
+
+        linkedList.deleteFirst();
+
+    }
+
+    public void show(){
+
+        linkedList.display();
+
+    }
+
 }
