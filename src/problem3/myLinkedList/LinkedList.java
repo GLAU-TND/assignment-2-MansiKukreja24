8ac76c55ed0e7dff4b03db1bc141a022c42dec1f
MansiKukreja24 <55954224+MansiKukreja24@users.noreply.github.com>
2020-03-31 19:13:39 +0530

    Create LinkedList.java

---
+++ src/problem3/myLinkedList/LinkedList.java
@@ -0,0 +1,55 @@
+package problem3.myLinkedList;
+
+
+
+import problem3.node.Node;
+
+import problem3.student.Student;
+
+
+
+public class LinkedList {
+
+    private Node first;
+
+    private Node last;
+
+
+
+    public void addLast(Student student){
+
+        Node node = new Node(student);
+
+        if(first==null){
+
+            first=last=node;
+
+            return;
+
+        }
+
+        last.setNext(node);
+
+        last=node;
+
+
+
+        var current = first;
+
+        while(current!=null){
+
+            if(node.getData().getRollNo()<current.getData().getRollNo()) {
+
+                var temp = node.getData();
+
+                node.setData(current.getData());
+
+                current.setData(temp);
+
+            }
+
+            current= current.getNext();
+
+        }
+
+    }
